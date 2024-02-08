25da5661f01c3df97477e20c7d11fd97b629c589
Marco Neumann <marco@crepererum.net>
2024-02-07 22:21:43 -0500

    base64 serde

--- src/proto.rs
+++ src/proto.rs
@@ -1,3 +1,4 @@
+use base64::prelude::*;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -64,6 +65,81 @@ impl<'de> serde::Deserialize<'de> for KdfVersion {
     }
 }
 
+#[derive(PartialEq, Eq, Hash)]
+pub struct Base64String(Box<[u8]>);
+
+impl Base64String {
+    pub fn base64(&self) -> String {
+        BASE64_STANDARD.encode(self.0.as_ref())
+    }
+}
+
+impl std::fmt::Debug for Base64String {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.base64())
+    }
+}
+
+impl std::fmt::Display for Base64String {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.base64())
+    }
+}
+
+impl From<Vec<u8>> for Base64String {
+    fn from(value: Vec<u8>) -> Self {
+        Self(value.into())
+    }
+}
+
+impl From<&[u8]> for Base64String {
+    fn from(value: &[u8]) -> Self {
+        Self(value.into())
+    }
+}
+
+impl<const N: usize> From<[u8; N]> for Base64String {
+    fn from(value: [u8; N]) -> Self {
+        Self(value.into())
+    }
+}
+
+impl<const N: usize> From<&[u8; N]> for Base64String {
+    fn from(value: &[u8; N]) -> Self {
+        Self(value.to_owned().into())
+    }
+}
+
+impl AsRef<[u8]> for Base64String {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
+impl serde::Serialize for Base64String {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.base64())
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Base64String {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        use serde::de::Error;
+
+        let s = String::deserialize(deserializer)?;
+        let data = BASE64_STANDARD
+            .decode(&s)
+            .map_err(|e| D::Error::custom(e))?;
+        Ok(Self(data.into()))
+    }
+}
+
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SaltServiceRequest {
@@ -81,7 +157,7 @@ pub struct SaltServiceResponse {
 
     pub kdf_version: KdfVersion,
 
-    pub salt: String,
+    pub salt: Base64String,
 }
 
 #[cfg(test)]
@@ -102,6 +178,12 @@ mod tests {
     fn test_roundtrip_kdf_version() {
         assert_roundtrip(KdfVersion::Bcrypt);
         assert_roundtrip(KdfVersion::Argon2id);
+    }
+
+    #[test]
+    fn test_roundtrip_base64trip() {
+        assert_roundtrip(Base64String::from(b""));
+        assert_roundtrip(Base64String::from(b"foo"));
     }
 
     #[track_caller]
