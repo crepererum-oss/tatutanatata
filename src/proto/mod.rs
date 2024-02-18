f1c56135df8ef4fee7d6e552df4fcd98f8dc7ef7
Marco Neumann <marco@crepererum.net>
2024-02-18 12:59:00 +0100

    split off constants proto

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -1,42 +1,19 @@
 use anyhow::Result;
 use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
 
-use self::binary::{Base64String, Base64Url};
+use self::{
+    binary::{Base64String, Base64Url},
+    constants::{Format, Null},
+};
 
 pub(crate) mod binary;
+pub(crate) mod constants;
 
 #[cfg(test)]
 mod testing;
 
 pub(crate) trait Entity {
     fn id(&self) -> &str;
-}
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub(crate) struct Format<const F: u8>;
-
-impl<const F: u8> serde::Serialize for Format<F> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&F.to_string())
-    }
-}
-
-impl<'de, const F: u8> serde::Deserialize<'de> for Format<F> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        let f: u8 = s.parse().map_err(D::Error::custom)?;
-        if f == F {
-            Ok(Self)
-        } else {
-            Err(D::Error::custom(format!("invalid format: {f}")))
-        }
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -69,18 +46,6 @@ impl<'de> serde::Deserialize<'de> for KdfVersion {
             "1" => Ok(Self::Argon2id),
             s => Err(D::Error::custom(format!("invalid KDF version: {s}"))),
         }
-    }
-}
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub(crate) struct Null;
-
-impl serde::Serialize for Null {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_none()
     }
 }
 
@@ -431,16 +396,6 @@ pub(crate) struct MailDetailsBlob {
 mod tests {
     use super::testing::{assert_deser_error, assert_roundtrip};
     use super::*;
-
-    #[test]
-    fn test_roundtrip_format() {
-        assert_roundtrip(Format::<0>);
-        assert_roundtrip(Format::<1>);
-        assert_roundtrip(Format::<2>);
-        assert_roundtrip(Format::<255>);
-
-        assert_deser_error::<Format<1>>(r#""0""#, "invalid format: 0");
-    }
 
     #[test]
     fn test_roundtrip_kdf_version() {
